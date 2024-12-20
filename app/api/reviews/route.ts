a29b6f80965cce99ad926f02e2f70f1f8ec10793
Noah Medoff <noahmedoff@gmail.com>
2024-12-19 17:10:56 -0800

    troubleshooting reviews route fetch

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -21,31 +21,32 @@ export interface Review {
 
 export async function GET() {
     try {
-        await signInAnonymously(auth);
-        const reviewsSnapshot = await getDocs(collection(db, "reviews"));
-        const reviewsArray: Review[] = [];
+        // await signInAnonymously(auth);
+        // const reviewsSnapshot = await getDocs(collection(db, "reviews"));
+        // const reviewsArray: Review[] = [];
 
-        reviewsSnapshot.forEach((reviewDoc) => {
-            const firestoreData = reviewDoc.data() as { review: FirestoreReviewData } | undefined;
+        // reviewsSnapshot.forEach((reviewDoc) => {
+        //     const firestoreData = reviewDoc.data() as { review: FirestoreReviewData } | undefined;
 
-            if (firestoreData && firestoreData.review) {
-                const reviewData = firestoreData.review;
-                const date = reviewData.datePosted;
+        //     if (firestoreData && firestoreData.review) {
+        //         const reviewData = firestoreData.review;
+        //         const date = reviewData.datePosted;
 
-                reviewsArray.push({
-                    id: reviewDoc.id,
-                    review: {
-                        datePosted: date,
-                        reviewText: reviewData.reviewText,
-                        name: reviewData.name,
-                    },
-                });
-            } else {
-                console.warn("Review document is missing data:", reviewDoc.id, firestoreData);
-            }
-        });
+        //         reviewsArray.push({
+        //             id: reviewDoc.id,
+        //             review: {
+        //                 datePosted: date,
+        //                 reviewText: reviewData.reviewText,
+        //                 name: reviewData.name,
+        //             },
+        //         });
+        //     } else {
+        //         console.warn("Review document is missing data:", reviewDoc.id, firestoreData);
+        //     }
+        // });
 
-        return NextResponse.json(reviewsArray);
+        // return NextResponse.json(reviewsArray);
+        return NextResponse.json("meow")
     } catch (error: any) {
         console.error("Error fetching reviews:", error);
         return NextResponse.json({ error: error.message }, { status: 500 });
