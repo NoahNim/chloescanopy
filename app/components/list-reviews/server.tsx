141adba49e84e19cce41ed478af5e613129674b8
Noah Medoff <noahmedoff@gmail.com>
2024-12-19 18:05:21 -0800

    fixed all errors. code cleanup

--- app/components/list-reviews/server.tsx
+++ app/components/list-reviews/server.tsx
@@ -9,15 +9,3 @@ export interface Review {
         name: string;
     };
 }
-
-// export default async function ListReviewsServer() {
-//     const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-//         ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-//         : 'http://localhost:3000'; // Fallback for local development
-//     const res = await fetch(`${baseUrl}/api/reviews`);
-//     if (!res.ok) {
-//         throw new Error(`Failed to fetch reviews: ${res.status} ${res.statusText}`);
-//     }
-//     const reviews: Review[] = await res.json();
-//     return <ListReviews />;
-// }
