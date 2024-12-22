804ec2fa0d11391ff159e9875a3c7c3213e8880c
Noah Medoff <noahmedoff@gmail.com>
2024-12-22 13:41:03 -0800

    render test google form on camps page

---
+++ app/camps/page.tsx
@@ -0,0 +1,6 @@
+export default function Camps() {
+
+    return (
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVU8hHzqFs-40-jqRCdBKbK3neh1UUFUS9B6SBKAWFNoqPg/viewform?embedded=true" width="640" height="821">Loading…</iframe>
+    )
+}
