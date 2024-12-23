cc30c9a588fd82613aaa489a9a6687d4d4203852
Noah Medoff <noahmedoff@gmail.com>
2024-12-22 17:03:46 -0800

    remove homepage photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Image src="/images/image0.jpeg" alt="Image error" height="500" width="100" />
+      {/* <Image src="/images/image0.jpeg" alt="Image error" height="5000" width="1000" /> */}
     </div>
   );
 }
