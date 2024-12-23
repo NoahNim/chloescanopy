623545eb3cd574f3878cb96413bee22474af13f2
Noah Medoff <noahmedoff@gmail.com>
2024-12-22 17:01:02 -0800

    add homepage photo

--- app/camps/page.tsx
+++ app/camps/page.tsx
@@ -1,6 +1,5 @@
 export default function Camps() {
 
     return (
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVU8hHzqFs-40-jqRCdBKbK3neh1UUFUS9B6SBKAWFNoqPg/viewform?embedded=true" width="640" height="821">Loading…</iframe>
-    )
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfNUWWMK4NuEAqIcFNfEA2x7EmPpUDNy_Pl5JXKauxCsHqhPg/viewform?embedded=true" width="640" height="700">Loading…</iframe>)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
+      <Image src="/images/image0.jpeg" alt="Image error" height="500" width="100" />
     </div>
   );
 }
