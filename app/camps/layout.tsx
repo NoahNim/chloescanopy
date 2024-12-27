90c127c457a70ebe03687fd5fab1efbc8e7173b1
Noah Medoff <noahmedoff@gmail.com>
2024-12-26 22:34:49 -0800

    add images and some styling

--- app/camps/layout.tsx
+++ app/camps/layout.tsx
@@ -4,7 +4,7 @@ export default function CampsLayout({
     children,
 }: { children: React.ReactNode }) {
     return (
-        <div className="max-w-screen-xl mx-auto p-4 background-color bg-teal-300">
+        <div className="max-w-screen-xl mx-auto p-2 background-color bg-teal-300 w-screen object-contain">
             {children}
         </div>
     )
