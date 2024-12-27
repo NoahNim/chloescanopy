965a8f37ab61aee5477ce4b35f2333f1d2f62c81
Noah Medoff <noahmedoff@gmail.com>
2024-12-27 00:23:01 -0800

    make navigation a green background color and give it that color in the mobile dropdown menu as well

--- app/components/nav/layout.tsx
+++ app/components/nav/layout.tsx
@@ -5,7 +5,7 @@ export default function NavLayout({
 }: { children: React.ReactNode }) {
     return (
         <div>
-            <nav className="max-w-screen-xl mx-auto p-4">
+            <nav>
                 {children}
             </nav>
         </div>
--- app/components/nav/page.tsx
+++ app/components/nav/page.tsx
@@ -13,7 +13,7 @@ export default function Nav() {
     }
 
     return (
-        <div className="">
+        <div className="bg-green-500">
             <div className="flex justify-center h-16">
                 <ul className="hidden md:flex flex-row space-x-80">
                     <li className="basis-1/4"><Link href={('/')}>Home</Link></li>
@@ -21,12 +21,12 @@ export default function Nav() {
                     <li className="basis-1/4"><Link href={('/reviews')}>Reviews</Link></li>
                 </ul>
             </div>
-            <div className="flex absolute top-4 left-1">
+            <div className="flex absolute top-4 left-1 bg-green-500">
                 {isMenuOpen ? (<ul className={`transition-all duration-300 ease-in-out flex-row space-y-2`}>
+                    <li><button onClick={toggleMenu}>X</button> </li>
                     <li className="basis-1/4"><Link href={('/')}>Home</Link></li>
                     <li className="basis-1/4"><Link href={('/camps')}>Camps</Link></li>
                     <li className="basis-1/4"><Link href={('/reviews')}>Reviews</Link></li>
-                    <li><button onClick={toggleMenu}>CloseMenu</button> </li>
                 </ul>) : (
                     <button className="md:hidden" onClick={toggleMenu}>Menu</button>
                 )}
