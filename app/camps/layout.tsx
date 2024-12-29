9ea2d1e9e42a93d963473d3b7407309415d2deba
Noah Medoff <noahmedoff@gmail.com>
2024-12-29 09:58:04 -0800

    add transparency to camps main background

--- app/camps/layout.tsx
+++ app/camps/layout.tsx
@@ -4,7 +4,7 @@ export default function CampsLayout({
     children,
 }: { children: React.ReactNode }) {
     return (
-        <div className="max-w-screen-xl mx-auto p-2 background-color bg-teal-300 w-screen object-contain">
+        <div className="max-w-screen-xl mx-auto p-2 bg-teal-300/50 w-screen object-contain">
             {children}
         </div>
     )
