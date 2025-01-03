40021f15a445638a30f2723ff22264bfa42d2eb1
Noah Medoff <noahmedoff@gmail.com>
2025-01-02 21:02:47 -0800

    add sign up links

--- app/camps/page.tsx
+++ app/camps/page.tsx
@@ -20,6 +20,11 @@ export default function Camps() {
                     <p>
                         Discover what the beach has to offer during a negative low tide. We&apos;ll observe tide pool creatures, practice proper beach etiquette, walk the shoreline from park to park, and share stories of our native lands and the Salish Sea.
                     </p>
+                    <div className="text-center m-3">
+                        <b>
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfNUWWMK4NuEAqIcFNfEA2x7EmPpUDNy_Pl5JXKauxCsHqhPg/viewform" target="_blank" className="bg-lime-500/90 border-solid rounded p-2">Sign Up Here!</a>
+                        </b>
+                    </div>
                 </div>
                 <div className="bg-lime-300/90 border-solid rounded p-4 flex flex-col">
                     <div className="flex justify-center border-solid rounded">
@@ -35,6 +40,11 @@ export default function Camps() {
                     <p>
                         We&apos;ll be playing outside, exploring the woods, enjoying time at the park, splashing around in the water, playing group games, and doing arts and crafts. Don&apos;t forget to bring a swimsuit and a towel, as we&apos;ll be heading to the water park!
                     </p>
+                    <div className="text-center m-3">
+                        <b>
+                            <a href="https://forms.gle/eAHDqNVVbvREs8DRA" target="_blank" className="bg-lime-500/90 border-solid rounded p-2">Sign Up Here!</a>
+                        </b>
+                    </div>
                 </div>
                 <div className="bg-lime-300/90 border-solid rounded p-4 flex flex-col">
                     <div className="flex justify-center border-solid rounded">
@@ -50,6 +60,11 @@ export default function Camps() {
                     <p>
                         Discover what the beach has to offer during a negative low tide. We&apos;ll observe tide pool creatures, practice proper beach etiquette, walk the shoreline from park to park, and hear stories about the Salish Sea.
                     </p>
+                    <div className="text-center m-3">
+                        <b>
+                            <a href="https://forms.gle/GacYNMuc1xEPgmy19" target="_blank" className="bg-lime-500/90 border-solid rounded p-2">Sign Up Here!</a>
+                        </b>
+                    </div>
                 </div>
                 <div className="bg-lime-300/90 border-solid rounded p-4 flex flex-col">
                     <div className="flex justify-center border-solid rounded">
@@ -65,6 +80,11 @@ export default function Camps() {
                     <p>
                         Just like our other previous Create and explore camp, We&apos;ll be playing outside, exploring the woods, enjoying time at the park, splashing around in the water, playing group games, and doing arts and crafts. Don&apos;t forget to bring a swimsuit and a towel, as we&apos;ll be heading to the water park!
                     </p>
+                    <div className="text-center m-3">
+                        <b>
+                            <a href="https://forms.gle/bKpkeYFPDv1TXfrY8" target="_blank" className="bg-lime-500/90 border-solid rounded p-2">Sign Up Here!</a>
+                        </b>
+                    </div>
                 </div>
             </div>
         </div>
