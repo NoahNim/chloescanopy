bd17a8f0a7382d2409097b0ed5b331ca2df64edc
Noah Medoff <noahmedoff@gmail.com>
2025-01-03 13:48:38 -0800

    create and render pricing component

---
+++ app/components/swim-components/Pricing.tsx
@@ -0,0 +1,19 @@
+export default function Pricing() {
+    return (
+        <div className="bg-lime-300/90 border-solid rounded p-4 flex flex-col m-3">
+            <h1 className="text-center font-bold text-2xl"><b>Pricing</b></h1>
+            <h2 className="text-center font-bold text-xl"><b>Group Class 3-4 students</b></h2>
+            <ul className="list-none p-2 text-center">
+                <li><b>Single Class:</b> $55</li>
+                <li><b>Six Classes:</b> $270</li>
+                <li><b>Make Up Class:</b> $15</li>
+            </ul>
+            <h2 className="text-center font-bold text-xl"><b>Private Lessons</b></h2>
+            <ul className="list-none p-2 text-center">
+                <li><b>15 minutes:</b> $55</li>
+                <li><b>30 minutes:</b> $100</li>
+                <li>$10/per additional student</li>
+            </ul>
+        </div>
+    )
+}
--- app/swimlessons/page.tsx
+++ app/swimlessons/page.tsx
@@ -1,5 +1,5 @@
 import SwimSessions from "../components/swim-components/SwimSessions"
-
+import Pricing from "../components/swim-components/Pricing"
 export default function SwimLessons() {
     const sessions = [
         { title: "Winter Session #1 Class Schedule", sundays: "Jan 5, 12, 19, 26, Feb 2, 9", mondays: "Jan 6, 13, 20, 27 Feb 3, 1", thursdays: "Jan 9, 16, 23, 30 Feb 6, 13" },
@@ -19,6 +19,9 @@ export default function SwimLessons() {
                 <SwimSessions title={sessions[0].title} sundays={sessions[0].sundays} mondays={sessions[0].mondays} thursdays={sessions[0].thursdays} />
                 <SwimSessions title={sessions[1].title} sundays={sessions[1].sundays} mondays={sessions[1].mondays} thursdays={sessions[1].thursdays} />
                 <SwimSessions title={sessions[2].title} sundays={sessions[2].sundays} mondays={sessions[2].mondays} thursdays={sessions[2].thursdays} />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-6 border-solid p-4 max-w-2xl mx-auto">
+                <Pricing />
             </div>
         </div>
     )
