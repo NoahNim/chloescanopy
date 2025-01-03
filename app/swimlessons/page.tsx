2fbb492cb3f29dc3c5a742918fd4ef2a33f7c4e9
Noah Medoff <noahmedoff@gmail.com>
2025-01-02 22:42:51 -0800

    add swim session info and render it in swim session components

--- app/swimlessons/page.tsx
+++ app/swimlessons/page.tsx
@@ -1,10 +1,23 @@
-// import SwimSessions from "../components/swim-sessions/SwimSessions"
+import SwimSessions from "../components/swim-components/SwimSessions"
 
 export default function SwimLessons() {
+    const sessions = [
+        { title: "Winter Session #1 Class Schedule", sundays: "Jan 5, 12, 19, 26, Feb 2, 9", mondays: "Jan 6, 13, 20, 27 Feb 3, 1", thursdays: "Jan 9, 16, 23, 30 Feb 6, 13" },
+        {
+            title: "Winter Session #2 Class Schedule", sundays: "Feb 23. Mar 2, 9, 16, 23, 30", mondays: "Feb 24. Mar 3, 10, 17, 24, 31", thursdays: "Feb 27, Mar 6, 13, 20, 27 Apr 3"
+        },
+        {
+            title: "Spring Class Schedule", sundays: "Apr 20, 27, May 4, 11 (2 weeks off) June 1, 8", mondays: "Apr 21, 28, May 5, 12 (2 weeks off) June 2, 9", thursdays: "Apr 4, May 1, 8, 15 (2 weeks off) June 5, 12"
+        },
+    ]
+
+
     return (
         <div className="border-solid rounded mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <h1>Swim Lessons</h1>
-            {/* <SwimSessions /> */}
+            <SwimSessions title={sessions[0].title} sundays={sessions[0].sundays} mondays={sessions[0].mondays} thursdays={sessions[0].thursdays} />
+            <SwimSessions title={sessions[1].title} sundays={sessions[1].sundays} mondays={sessions[1].mondays} thursdays={sessions[1].thursdays} />
+            <SwimSessions title={sessions[2].title} sundays={sessions[2].sundays} mondays={sessions[2].mondays} thursdays={sessions[2].thursdays} />
         </div>
     )
 }
