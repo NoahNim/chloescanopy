5fd95f07a2791e754e3a3c29635a98715a0a054b
Noah Medoff <noahmedoff@gmail.com>
2025-01-12 13:26:02 -0800

    added home page image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full max-w-md mx-auto p-4 bg-lime-200/90 border-solid rounded mt-4">
-      {/* <Image src="/images/image0.jpeg" alt="Image error" height="5000" width="1000" /> */}
+      <Image src="/images/image0.jpeg" alt="Image error" width={400}
+        height={300}
+        className="rounded object-cover w-full h-[300px]" />
       <h1 className="text-center text-2xl"><b>Chloe&apos;s Canopy</b></h1>
       <p className="text-center">Welcome to Chloe&apos;s Canopy! Extra-curricular programs for kids and adults to move their bodies and build confidence by learning to navigate a variety of environments, from  Water to Land to Mountain, Rain or Shine. We explore the world year-round! We thrive when equipped with the right tools, and here, you&apos;ll have fun discovering your outdoor abilities in a safe, nurturing space designed to help you develop the skills you need for your next adventur</p>
     </div>
